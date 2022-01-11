d04bf0b51bdd68e46fd80858807c1edaed2c314e
Roram <68767119+Roram-io@users.noreply.github.com>
2022-01-11 00:57:05 -0500

    Implemented Controller

--- src/main/java/com/nttdata/credits/service/CreditService.java
+++ src/main/java/com/nttdata/credits/service/CreditService.java
@@ -1,4 +1,19 @@
 package com.nttdata.credits.service;
 
-public class CreditService {
+import com.nttdata.credits.model.Credit;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface CreditService {
+    Flux<Credit> listCredits();
+
+    Mono<Credit> listById(String id);
+
+    Mono<Credit> listByHolder(String id);
+
+    Mono<Void> removeCredit(String id);
+
+    Mono<Credit> saveCredit(Credit credit);
+
+    Mono<Credit> updateCredit(Credit credit);
 }
